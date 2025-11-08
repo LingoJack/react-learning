6cebf22f7b4e103fe4d4516feef89b562e1529ba
lingojack <3065225677@qq.com>
2025-11-09 01:31:47 +0800

    2

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,8 +1,23 @@
+interface Human {
+  name: string
+}
+
 function App() {
 
+  const str: string = "WenJunjie"
+
+  const lover: Array<string> = [
+    
+  ]
+
+  const talk = (somebody: Array<string>): string => {
+    return "talk " + somebody.join(" and ")
+  }
+
   return (
-    <div>
-      
+    <div className="App">
+      <picture></picture>
+      <button onClick={() => { alert(str + " " + talk(lover)); }}> love </button>
     </div>
   )
 }
--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+// 找到 index.html 中的 root 渲染
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
